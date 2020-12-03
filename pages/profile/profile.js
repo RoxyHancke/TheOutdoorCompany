a079c4ed7adc7b29861614c4b0d8a6411c8207f0
Zena Wang <sihuawang.zena@gmail.com>
2020-12-03 21:51:56 +0800

    finshed profile page

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,15 +1,46 @@
-// pages/profile/profile.js
-const app = getApp()
+const app=getApp();
+
 Page({
-
-
   data: {
-
+    userInfo:[],
+    items:{},
   },
 
-  onLoad: function (options) {
-
+  onShow: function () {
+    const page = this
+  wx.getStorage({
+    key: 'userInfo',
+    success(res){
+      console.log("checkout user info",res.data);
+      page.setData({
+        userInfo: res.data
+      });
+    page.getUserInfo(res.data)
+    }
+  })
   },
 
+  getUserInfo:function(userInfo){
+    const userProfile = new wx.BaaS.TableObject("TOC_userInfo");
+    // set up the query
+      let query = new wx.BaaS.Query();
+      query.compare("userID","=",userInfo.id);
+      userProfile.setQuery(query)
+      .find()
+      .then(
+        (res)=>{
+          console.log("query results",res)
+          this.setData({
+            items: res.data.objects[0],
+          })
+        }
+      )
+  },
 
-})
+  editProfile: function(e){
+    wx.navigateTo({
+      url: `/pages/editProfile/editProfile?id=${this.data.items.id}`,
+    })
+  },
+
+})  
