d739826324cd80284d8c950e4f3112451cece42b
nefishastemir <nefise1203>
2020-12-05 16:14:15 +0800

    Details Page updated with Home Page

--- pages/details/details.js
+++ pages/details/details.js
@@ -12,8 +12,7 @@ Page({
         },
         ],
         activity: {},
-          
-
+        trips:{},
           comments:[{comment:""}],
           scrollInto: "",
           inputVal: "",
@@ -47,51 +46,51 @@ Page({
     // const comment_storyId="5fbd042933015444fc5031e8"
     console.log("detail page options", options);
     // console.log(this.data.options.id)
+                // UsergetInfo.expand(['userID']).get(options.tripId).then((res) => {
+                //   console.log("halaluya", res );
+                //   this.setData({
+                //     userInfo: res.data
+                //   });    
 
-    Activities.expand(['tripOwner']).get(options.tripId).then((res) => {
-              console.log("halaluya");
-              console.log(res);
-              this.setData({
-                activity: res.data
-              });
-              
-        });
-        // // Getting all the reviews with movie_id == options.id
-        // // set up the query
-        // let query = new wx.BaaS.Query();
-        // query.compare("tripOwner", "=", options);
-        // // console.log("jhbjhbj",options_id);
-        // const that = this;
-        // Activities.setQuery(query)
-        //       .expand(["userId", "tripOwner"])
+                // });
+              // UsergetInfo.expand(['userID']).get(options.tripId).then((res) => {
+  
+              //     this.setData({ 
+              //       image:res.data.profilePicture,
+              //       name:res.data.nickName,
+              //     });
+              // });
+
+              Activities.expand(['tripOwner']).get(options.tripId).then((res) => {
+                      console.log("halaluya", res );
+                      this.setData({
+                        activity: res.data
+                      });
+            });
+                  const trips = res.data.objects;
+                  const pictures = [];
+                  for(let i = 0; i < trips.length; i++) {
+                    const picturesForOneTrip = trips[i].picture;
+                    pictures.push(...picturesForOneTrip);
+                  }
+            
+                  console.log("hayirdir insallah ",res);
+                  const remappedTrips = trips.map((trip) => {
+                    return {
+                      ...trip,
+                      tripStartDate: moment(trip.tripStartDate).format('DD/MM/YYYY')
+                    }
+                  })
+                  console.log('ppp', pictures);
+                  this.setData({
+                        activity: remappedTrips,
+                        pictures: pictures
+                  }); 
+                  console.log(that.t_picture) 
+                  console.log(res);
+              }, 
+      
     
-        //     .find()
-        //     .then((res) => {
-        //           const trips = res.data.objects;
-        //           const pictures = [];
-        //           for(let i = 0; i < trips.length; i++) {
-        //             const picturesForOneTrip = trips[i].picture;
-        //             pictures.push(...picturesForOneTrip);
-        //           }
-        //           const remappedTrips = trips.map((trip) => {
-        //             return {
-        //               ...trip,
-        //               tripStartDate: moment(trip.tripStartDate).format('DD/MM/YYYY')
-        //             }
-        //           })
-
-        //           console.log('ppp', pictures);
-        //           this.setData({
-        //           activities: remappedTrips,
-        //           pictures: pictures
-        //           }); 
-                
-        //           // console.log(that.t_picture) 
-        //           console.log(res);
-        //       }, 
-        //       );
-              
-        },
         changeIndicatorDots() {
           this.setData({
             indicatorDots: !this.data.indicatorDots
