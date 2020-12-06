593062ab2e32ba3a4c902a0e26c1ba83afa8386f
Zena Wang <sihuawang.zena@gmail.com>
2020-12-06 14:10:24 +0800

    1- link home page with detail page; 2-loaded trips in profile and finished the picker feature in editing profile.

--- pages/editProfile/editProfile.js
+++ pages/editProfile/editProfile.js
@@ -2,6 +2,7 @@
 Page({
   data: {
     id:"",
+    tripTypeOptions:['Short hiking','Long hiking','Camping','Glamping','Trail running','Mountaineering','Biking','Others']
   },
 
   onLoad: function (options) {
@@ -11,6 +12,33 @@ Page({
     })
   },
 
+  bindPickerInterest1:function(e){
+    const page = this
+   console.log('interest 1 picked', e),
+   console.log('interest picked name',this.data.tripTypeOptions[e.detail.value])
+   this.setData({
+     interest1: this.data.tripTypeOptions[e.detail.value]
+   })
+  },
+
+  bindPickerInterest2:function(e){
+    const page = this
+   console.log('interest 2 picked', e),
+   console.log('interest picked name',this.data.tripTypeOptions[e.detail.value])
+   this.setData({
+     interest2: this.data.tripTypeOptions[e.detail.value]
+   })
+  },
+
+  bindPickerInterest3:function(e){
+    const page = this
+   console.log('interest 3 picked', e),
+   console.log('interest picked nname',this.data.tripTypeOptions[e.detail.value])
+   this.setData({
+     interest3: this.data.tripTypeOptions[e.detail.value]
+   })
+  },
+
 formSubmit: function(e){
   console.log("formsubmit",e);
   const page = this;
@@ -18,9 +46,9 @@ formSubmit: function(e){
   //get the data table
   const UserProfile = new wx.BaaS.TableObject("TOC_userInfo");
   const userProfile = UserProfile.getWithoutData(this.data.id);
-  userProfile.set("interest1",e.detail.value.interest1);
-  userProfile.set("interest2",e.detail.value.interest2);
-  userProfile.set("interest3",e.detail.value.interest3);
+  userProfile.set("interest1",this.data.interest1);
+  userProfile.set("interest2",this.data.interest2);
+  userProfile.set("interest3",this.data.interest3);
   userProfile.set("shortBio",e.detail.value.shortBio);
   userProfile.update().then(
     (res)=>{
@@ -33,4 +61,6 @@ formSubmit: function(e){
         url: '/pages/profile/profile',
       })
     }
+
+    
 })
